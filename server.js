ee65b5bf763f556b0dccf2e6bae08fa00401a032
erinç erensoy <erincerensoy@ip-192-168-0-11.eu-west-1.compute.internal>
2022-04-02 23:58:56 +0300

    modified buildspec

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Hellooo world</h1> \n');
+  res.send('<h1 style="color:green;">Helloooo world</h1> \n');
 });
 
 app.listen(PORT, HOST);
